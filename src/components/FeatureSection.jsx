a0d7e44903a669034f1c37e6c1fb70cd24547c34
tharushadinuth(pro) <tharushadinuth21@gmail.com>
2024-12-09 22:01:00 +0530

    create about us page

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import CountUp from "react-countup";
+import './FeatureSection.css';
+
 
 const features = [
-  { icon: "fa-users", value: "3453", label: "Happy Customers" },
-  { icon: "fa-check", value: "4234", label: "Project Done" },
-  { icon: "fa-award", value: "3123", label: "Awards Win" },
-  { icon: "fa-users-cog", value: "1831", label: "Expert Workers" },
+  { icon: "fa-users", value: "99", label: "Happy Customers" },
+  { icon: "fa-check", value: "100", label: "Project Done" },
+  { icon: "fa-award", value: "1", label: "Awards Win" },
+  { icon: "fa-users-cog", value: "10", label: "Expert Workers" },
 ];
 
 const FeatureSection = () => (
   <div className="container-xxl py-5">
+     <span className="centered-text" >   Welcome to RIVIDCO, where we believe solar energy isn’t just about saving the planet—it’s about delivering premium solutions that last. We are committed to providing high-quality solar products and services, ensuring every system we install is built to perform reliably for years to come.</span>
     <div className="container">
+   
       <div className="row g-5">
+     
         {features.map((feature, index) => (
           <div className="col-md-6 col-lg-3 wow fadeIn" data-wow-delay={`${index * 0.2}s`} key={index}>
             <div className="d-flex align-items-center mb-4">
@@ -23,7 +28,6 @@ const FeatureSection = () => (
               </h1>
             </div>
             <h5 className="mb-3">{feature.label}</h5>
-            <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
           </div>
         ))}
       </div>
